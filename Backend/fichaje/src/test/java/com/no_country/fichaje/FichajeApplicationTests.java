392fd612bc16ee399094ef098c92efb16f4bc240
emanuel peracchia <emanuelperacchia1@gmail.com>
2025-02-17 12:18:44 -0300

    primer paso

---
+++ Backend/fichaje/src/test/java/com/no_country/fichaje/FichajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no_country.fichaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
